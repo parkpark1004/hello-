235b41ce8c12e67f9ff11ff7d27805d270fdd9bd
parkpark1004 <chaeyeon503@icloud.com>
2025-03-14 17:24:47 +0900

    AI프로그래밍및실습2주차과제입니다.

---
+++ 202312635 해양학전공 박채연 AI프로그래밍및실습 2주차 과제.py
@@ -0,0 +1,43 @@
+#LAB2-1
+
+print('나의 이름은:','홍길동')
+print('나의 나이는:','27')
+print('나의 키는',179,'cm입니다.')
+print('10+20=',10+20)
+print('10*20=',10*20)
+
+#LAB2-4
+
+name='전우치'
+print('나의 이름은:',name)
+
+age='27'
+print('나의 나이는:',age)
+
+height='179'
+print('나의 키는:',height,'cm 입니다.')
+
+sum=10+20
+print('10+20=',sum)
+
+mult=10*20
+print('10*20=',mult)
+
+#LAB2-6
+width=20
+height=40
+area=width*height
+print('사각형의 면적',area)
+
+#LAB2-7
+print(123*456)
+print(1357+2468)
+print(5**4)
+print(10/4)
+print(10//5)
+print(10%5)
+
+print(5%2)
+
+print(2**(1/2))
+print(3**(1/2))
